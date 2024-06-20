0faabd267f6f9aa427d1e920310f1139bc93dead
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-20 21:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 let a=prompt("Enter a number between 1 to 20: ");
 if(a%3==0){
-	alert("Fizz");
+	alert(`${a} \nFizz`);
 }
 else if(a%5==0){
-	alert("Buzz");
+	alert(`${a} \nBuzz`);
 }
 else if(a%3==0 && a%5==0){
-	alert("FizzBuzz");
+	alert(`${a} \nFizzBuzz`);
 }
